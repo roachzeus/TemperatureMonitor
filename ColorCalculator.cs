57ccadfbbcac2e23647516f256885b3a4fc08c98
roach <roach@KRISU-PC>
2024-04-07 14:19:07 +0300

    Add project files.

---
+++ ColorCalculator.cs
@@ -0,0 +1,39 @@
+﻿namespace TemperatureMonitor
+{
+    internal class ColorCalculator
+    {
+        public Color computeColor(double temp)
+        {
+            int low = 20;
+            int max = 100;
+            int mid = 60;
+            int range = max - low;
+
+            if (temp < 0)
+            {
+                return Color.FromArgb(255, 255, 255); // when all else fails
+            }
+
+            if (temp <= low)
+            {
+                return Color.FromArgb(0, 192, 0);
+            }
+            else if (temp >= max)
+            {
+                return Color.FromArgb(192, 0, 0);
+            }
+            else if (temp >= mid) // orangish
+            {
+                return Color.FromArgb(192, (int)(((((max - temp) / 10) / 4) * 255) * 0.75), 0);
+            }
+            else if (temp < mid) // yellowgreen
+            {
+                return Color.FromArgb((int)(((1 - (((mid - temp) / 10) / 4)) * 255) * 0.75), 192, 0);
+            }
+            else
+            {
+                return Color.FromArgb(255, 255, 255); // when all else fails
+            }
+        }
+    }
+}
---
+++ Monitor/Monitor.cs
@@ -0,0 +1,89 @@
+﻿using LibreHardwareMonitor.Hardware;
+
+namespace TemperatureMonitor.Monitor
+{
+    public class Monitor
+    {
+        private bool shoudRun = true;
+        private Dictionary<String, float> valueData;
+        public Monitor()
+        {
+            this.valueData = new Dictionary<String, float>();
+        }
+
+        public void doWork()
+        {
+            while (shoudRun)
+            {
+
+            }
+        }
+
+        public void stopWork()
+        {
+            shoudRun = false;
+        }
+
+        public Dictionary<String, float> updateSensors(bool includeMobo)
+        {
+            Computer computer = new Computer
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMemoryEnabled = false,
+                IsMotherboardEnabled = includeMobo,
+                IsControllerEnabled = false,
+                IsNetworkEnabled = false,
+                IsStorageEnabled = false
+            };
+
+            computer.Open();
+            computer.Accept(new UpdateVisitor());
+            Dictionary<String, float> values = new Dictionary<String, float>();
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                
+                //Console.WriteLine("Hardware: {0}", hardware.Name);
+                foreach (IHardware subhardware in hardware.SubHardware)
+                {
+                   //Console.WriteLine("\tSubhardware: {0}", subhardware.Name);
+
+                    foreach (ISensor sensor in subhardware.Sensors)
+                    {
+                        if (sensor.SensorType.ToString() != "Temperature")
+                        {
+                            continue;
+                        }
+                        values.Add(sensor.Name, sensor.Value ?? -1);
+                        //Console.WriteLine("\t\tSensor: {0}, value: {1}", sensor.Name, sensor.Value);
+                    }
+                }
+                
+                foreach (ISensor sensor in hardware.Sensors)
+                {
+                    //Console.WriteLine("\tSensor: {0}, value: {1}, type: {2}", sensor.Name, sensor.Value, sensor.SensorType);
+
+                    if (sensor.SensorType.ToString() != "Temperature")
+                    {
+                        continue;
+                    }
+
+                    if (sensor.Hardware.HardwareType.ToString() == "Cpu")
+                    {
+                        values.Add(sensor.Name, sensor.Value ?? -1);
+                        //Console.WriteLine("\tSensor: {0}, value: {1}, type: {2}", sensor.Name, sensor.Value, sensor.SensorType);
+
+                    }
+                    else if (sensor.Hardware.HardwareType.ToString() == "GpuNvidia" || sensor.Hardware.HardwareType.ToString() == "GpuAmd")
+                    {
+                        values.Add(sensor.Name, sensor.Value ?? -1);
+                        //Console.WriteLine("\tSensor: {0}, value: {1}, type: {2}", sensor.Name, sensor.Value, sensor.SensorType);
+                    }
+                }
+            }
+
+            computer.Close();
+            return values;
+        }
+    }
+}
